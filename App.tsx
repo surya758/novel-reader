9e11f530988948abb34bd21aaf54312053d19960
Suryakant <skant758@gmail.com>
2024-07-17 17:57:51 +0530

    fix: warning removed

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import "./gesture-handler";
 import RootNav from "@src/navigation/RootNav";
 import QueryProvider from "@src/store/provider";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["Sending"]);
 
 export default function App() {
 	return (
